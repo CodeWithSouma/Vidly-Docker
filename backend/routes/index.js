83585695c9968f86207e98b4e1031585c66e95e5
Souma <souma.hitech@gmail.com>
2021-05-15 01:46:47 +0530

    map application code with container

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.send("<h1>Api is running!</h1>");
+  res.send("<h1>Api is running!!</h1>");
 });
 
 module.exports = router;
